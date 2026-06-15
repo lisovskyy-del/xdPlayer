e6078a645812be5020c0f332f82141b950c262cd
Denis Lisovskyy <lisovskyy041@gmail.com>
2026-06-15 13:54:54 +0300

    Added Serilog

--- xdPlayer.App/Program.cs
+++ xdPlayer.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using Serilog;
 
 namespace xdPlayer.App;
 
@@ -9,13 +10,36 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(
+                path: "logs/xdPlayer-.log",
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7
+            )
+            .CreateLogger();
 
+        try
+        {
+            Log.Information("Launching xdPlayer...");
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "App failed to launch");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
+            .UseWin32()
 #if DEBUG
             .WithDeveloperTools()
 #endif
