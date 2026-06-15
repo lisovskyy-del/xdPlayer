4c524cbbf2de443e57914431b1ca25bcce7bdd40
Denis Lisovskyy <lisovskyy041@gmail.com>
2026-06-15 11:17:12 +0300

    Dependency Injection, at first broken but other files will be added

--- xdPlayer.App/App.axaml.cs
+++ xdPlayer.App/App.axaml.cs
@@ -1,11 +1,16 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting.Internal;
+using System;
 
 namespace xdPlayer.App;
 
-public partial class App : Application
+public partial class App : Avalonia.Application
 {
+    public static IServiceProvider Services { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -13,11 +18,39 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        Services = services.BuildServiceProvider();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = Services.GetRequiredService<MainWindow>();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        // DB
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer("your_connection_string"));
+
+        // Repositories
+        services.AddScoped<ITrackRepository, TrackRepository>();
+        services.AddScoped<IPlaylistRepository, PlaylistRepository>();
+
+        // Services
+        services.AddScoped<ILibraryService, LibraryService>();
+        services.AddScoped<IPlayerService, PlayerService>();
+        services.AddScoped<IStatisticsService, StatisticsService>();
+
+        // ViewModels
+        services.AddTransient<MainViewModel>();
+        services.AddTransient<LibraryViewModel>();
+        services.AddTransient<PlayerViewModel>();
+
+        // Windows
+        services.AddSingleton<MainWindow>();
     }
 }
