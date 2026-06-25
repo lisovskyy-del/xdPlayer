9ee20be4e79d7e50af1d5b4e5580bb2502ec8bb5
Denis Lisovskyy <lisovskyy041@gmail.com>
2026-06-25 14:17:55 +0300

    w.i.p playlist service

--- xdPlayer.Application/Interfaces/IPlaylistService.cs
+++ xdPlayer.Application/Interfaces/IPlaylistService.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using xdPlayer.Domain.Entities;
 
 namespace xdPlayer.Application.Interfaces;
 
-internal interface IPlaylistService
+public interface IPlaylistService
 {
+    Task<IEnumerable<Playlist>> GetAllAsync();
+    Task<Playlist?> GetWithTracksAsync(int id);
+
+    Task<Playlist> CreateAsync(string name, string? description = null);
+
+    Task RenameAsync(int playlistId, string newName);
+    Task AddTrackAsync(int playlistId, int trackId);
+    Task RemoveTrackAsync(int playlistId, int trackId);
+
+    Task DeleteAsync(int playlistId);
 }
---
+++ xdPlayer.Application/Services/PlaylistService.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using xdPlayer.Application.Interfaces;
+using xdPlayer.Domain.Entities;
+using xdPlayer.Domain.Interfaces;
+
+namespace xdPlayer.Application.Services;
+
+public class PlaylistService : IPlaylistService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public PlaylistService(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    public async Task<IEnumerable<Playlist>> GetAllAsync()
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+        return await uow.Playlists.GetAllAsync();
+    }
+
+    public async Task<Playlist?> GetWithTracksAsync(int id)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+        return await uow.Playlists.GetWithTracksAsync(id);
+    }
+
+    public async Task<Playlist> CreateAsync(string name, string? description = null)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        var playlist = new Playlist
+        {
+            Name = name,
+            Description = description,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow,
+        };
+
+        await uow.Playlists.AddAsync(playlist);
+        await uow.SaveChangesAsync();
+        return playlist;
+    }
+
+    public async Task RenameAsync(int playlistId, string newName)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        var playlist = await uow.Playlists.GetByIdAsync(playlistId);
+        if (playlist == null) return;
+
+        playlist.Name = newName;
+        playlist.UpdatedAt = DateTime.UtcNow;
+
+        await uow.Playlists.UpdateAsync(playlist);
+        await uow.SaveChangesAsync();
+    }
+
+    public async Task AddTrackAsync(int playlistId, int trackId)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        var playlist = await uow.Playlists.GetWithTracksAsync(playlistId);
+        if (playlist == null) return;
+
+        if (playlist.PlaylistTracks.Any(pt => pt.TrackId == trackId)) return;
+
+        playlist.PlaylistTracks.Add(new PlaylistTrack
+        {
+            PlaylistId = playlistId,
+            TrackId = trackId,
+        });
+
+        playlist.UpdatedAt = DateTime.UtcNow;
+        await uow.SaveChangesAsync();
+    }
+
+    public async Task RemoveTrackAsync(int playlistId, int trackId)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        var playlist = await uow.Playlists.GetWithTracksAsync(playlistId);
+        if (playlist == null) return;
+
+        var playlistTrack = playlist.PlaylistTracks
+            .FirstOrDefault(pt => pt.TrackId == trackId);
+
+        if (playlistTrack == null) return;
+
+        playlist.PlaylistTracks.Remove(playlistTrack);
+        playlist.UpdatedAt = DateTime.UtcNow;
+        await uow.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int playlistId)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var uow = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+        var playlist = await uow.Playlists.GetByIdAsync(playlistId);
+        if (playlist == null) return;
+
+        await uow.Playlists.DeleteAsync(playlist);
+        await uow.SaveChangesAsync();
+    }
+}
