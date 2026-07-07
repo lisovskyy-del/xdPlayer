dafff3d7a8641815dfbd69eed1140eb6bf77e84e
Denis Lisovskyy <lisovskyy041@gmail.com>
2026-07-07 21:06:24 +0300

    Added hide in tray

--- xdPlayer.App/App.axaml.cs
+++ xdPlayer.App/App.axaml.cs
@@ -2,21 +2,34 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
+using System.Windows.Input;
+using xdPlayer.App.ViewModels;
 using xdPlayer.Application.Interfaces;
+using xdPlayer.Application.Models;
+using xdPlayer.Application.Services;
 using xdPlayer.Domain.Interfaces;
 using xdPlayer.Domain.Playback;
 using xdPlayer.Infrastructure.Data;
 using xdPlayer.Infrastructure.Repositories;
 using xdPlayer.Infrastructure.Services;
-using xdPlayer.App.ViewModels;
-using xdPlayer.Application.Models;
-using xdPlayer.Application.Services;
+using Avalonia.Threading;
 
 namespace xdPlayer.App;
+
+internal class ActionCommand : ICommand
+{
+    private readonly Action _action;
+    public ActionCommand(Action action) => _action = action;
+    public bool CanExecute(object? parameter) => true;
+    public void Execute(object? parameter) => _action();
+    public event EventHandler? CanExecuteChanged { add { } remove { } }
+}
 
 public partial class App : Avalonia.Application
 {
@@ -42,6 +55,51 @@ public partial class App : Avalonia.Application
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            desktop.ShutdownMode = Avalonia.Controls.ShutdownMode.OnExplicitShutdown;
+
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            desktop.MainWindow = mainWindow;
+
+            var iconUri = new Uri("avares://xdPlayer.App/Assets/xdPlayer.ico");
+
+            var restoreWindowAction = new Action(() =>
+            {
+                Dispatcher.UIThread.Post(() =>
+                {
+                    mainWindow.Show();
+                    mainWindow.WindowState = WindowState.Normal;
+                    mainWindow.ShowInTaskbar = true;
+                    mainWindow.Activate();
+                });
+            });
+
+            var trayIcons = new TrayIcons
+            {
+                new TrayIcon
+                {
+                    Icon = new WindowIcon(new Bitmap(AssetLoader.Open(iconUri))),
+                    ToolTipText = "xdPlayer",
+
+                    Menu = new NativeMenu
+                    {
+                        Items =
+                        {
+                            new NativeMenuItem("Exit")
+                            {
+                                Command = new ActionCommand(() =>
+                                {
+                                    Dispatcher.UIThread.Post(() =>
+                                    {
+                                        mainWindow.ForceShutdown();
+                                        desktop.Shutdown();
+                                    });
+                                })
+                            }
+                        }
+                    }
+                }
+            };
+
             desktop.Exit += async (_, _) =>
             {
                 var sessionService = Services.GetRequiredService<ListeningSessionService>();
@@ -69,11 +127,13 @@ public partial class App : Avalonia.Application
             var settings = await themeService.LoadSettingsAsync();
             themeService.ApplyAccentColor(settings.AccentColor);
 
-            desktop.MainWindow = Services.GetRequiredService<MainWindow>();
+
+            Avalonia.Application.Current!.SetValue(TrayIcon.IconsProperty, trayIcons);
         }
 
         base.OnFrameworkInitializationCompleted();
     }
+
 
     private void ConfigureServices(IServiceCollection services)
     {
--- xdPlayer.App/MainWindow.axaml.cs
+++ xdPlayer.App/MainWindow.axaml.cs
@@ -2,29 +2,47 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.VisualTree;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Linq;
-using xdPlayer.App.ViewModels;
-using xdPlayer.App.Views;
 
 namespace xdPlayer.App;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+    private bool _isShuttingDown = false;
 
-    public MainWindow(MainWindowViewModel vm)
+    public MainWindow(ViewModels.MainWindowViewModel vm)
     {
         InitializeComponent();
         DataContext = vm;
 
         Focusable = true;
 
+        Closing += MainWindow_Closing;
+
         AddHandler(PointerPressedEvent, OnGlobalPointerPressed, RoutingStrategies.Tunnel);
+    }
+
+    public MainWindow() : this(null!)
+    {
+    }
+
+    private void MainWindow_Closing(object? sender, WindowClosingEventArgs e)
+    {
+        if (_isShuttingDown)
+        {
+            return;
+        }
+
+        e.Cancel = true;
+
+        Hide();
+        ShowInTaskbar = false;
+    }
+
+    public void ForceShutdown()
+    {
+        _isShuttingDown = true;
+        Close();
     }
 
     private void OnGlobalPointerPressed(object? sender, PointerPressedEventArgs e)
